5d79b290bb083eb12b21377b08e3e6d76417b62b
SCshredder17 <57152954+SCshredder17@users.noreply.github.com>
2020-10-25 01:07:33 -0700

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -218,7 +218,7 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate == CAR.RAV4_TSS2:
       stop_and_go = True
-      ret.safetyParam = 55
+      ret.safetyParam = 53
       ret.wheelbase = 2.68986
       ret.steerRatio = 14.3
       tire_stiffness_factor = 0.7933
