36f101d3e20ae90f6fef57f5b437e80e02f01fbc
birdman6450 <67883352+birdman6450@users.noreply.github.com>
2021-01-05 11:21:49 -0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -204,6 +204,27 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
       ret.lateralTuning.pid.kf = 0.00007818594
+      
+   elif candidate == CAR.COROLLA_TSS2:
+      stop_and_go = True
+      ret.minSpeedCan = 0.375
+      ret.safetyParam = 50 
+      ret.wheelbase = 2.63906
+      ret.steerRatio = 15.33
+      tire_stiffness_factor = 0.996  # not optimized yet
+      ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
+      ret.steerActuatorDelay = 0.6
+      ret.steerLimitTimer = 5.0
+      ret.lateralTuning.init('indi')
+      ret.lateralTuning.indi.innerLoopGainBP = [18, 22, 26]
+      ret.lateralTuning.indi.innerLoopGainV = [5, 10, 15]
+      ret.lateralTuning.indi.outerLoopGainBP = [18, 22, 26]
+      ret.lateralTuning.indi.outerLoopGainV = [4, 9, 14.99]
+      ret.lateralTuning.indi.timeConstantBP = [18, 22, 26]
+      ret.lateralTuning.indi.timeConstantV = [2, 4, 5.5]
+      ret.lateralTuning.indi.actuatorEffectivenessBP = [18, 22, 26]
+      ret.lateralTuning.indi.actuatorEffectivenessV = [5, 10, 15]
+   
 
     elif candidate in [CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2]:
       stop_and_go = True
